861e21a515c40e57519b6a97781bfed78ab1d341
Jamie <G00419525@atu.ie>
2025-01-04 23:08:54 +0000

    added some code so react could communicate with the api.

--- src/main/java/ie/atu/weightapplication/WeightClassController.java
+++ src/main/java/ie/atu/weightapplication/WeightClassController.java
@@ -1,12 +1,14 @@
 package ie.atu.weightapplication;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/weight-class")
 public class WeightClassController {
