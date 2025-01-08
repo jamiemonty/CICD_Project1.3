46d9377479ab6a1a30d4df41acb0ff1d8066a553
Jamie <G00419525@atu.ie>
2025-01-08 00:38:22 +0000

    Cleaned up code, added some code to handle get requests to certain parts of the MySQL weight-class table.

--- src/main/java/ie/atu/weightapplication/WeightClassRepository.java
+++ src/main/java/ie/atu/weightapplication/WeightClassRepository.java
@@ -1,6 +1,13 @@
 package ie.atu.weightapplication;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface WeightClassRepository extends JpaRepository<WeightClass, Long> {
+    @Query("SELECT w.weightRange FROM WeightClass w")
+    List<String> findAllWeightRanges();
 }
